24cdede710614745e814517537fc7bb9c407d601
Robyn Smith <robynsmith21@hotmail.com>
2023-08-03 02:52:41 +0100

    added post and put routes for category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,11 +14,37 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  // create a new category --------------------change categoryData const name?-have already changed err to error
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+  if (!categoryData) {
+    res.status(404).json({ message: 'No Category found with that id'}); //------change string
+    return;
+  }
+
+  res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
