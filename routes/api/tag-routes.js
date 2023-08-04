25619f4cb0e53130389850809cdc68ea5455cdeb
Robyn Smith <robynsmith21@hotmail.com>
2023-08-04 21:48:38 +0100

    created get request in tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,15 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      //add book as a second model to JOIN with ----- jsn comment--- change tagData name changed err to error
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get('/:id', (req, res) => {
