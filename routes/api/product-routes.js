757cb7082fcf34924fe8e1aeb89889742f16f5ff
Robyn Smith <robynsmith21@hotmail.com>
2023-08-04 22:35:55 +0100

    added delete request to product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -153,6 +153,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No Product found with that id'}); //change error message
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
