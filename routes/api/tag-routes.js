b2286671379afb2290c40b044f4dfeb24b15b086
Robyn Smith <robynsmith21@hotmail.com>
2023-08-05 01:52:56 +0100

    added post request tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,6 +38,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
