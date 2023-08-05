afdd54e25664a104f2359e4f777116b8ed0e9fee
Robyn Smith <robynsmith21@hotmail.com>
2023-08-05 01:55:03 +0100

    put request and delete request for tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -48,10 +48,41 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+      {where: { id: req.params.id}
+    });
+    if(!categoryData){
+      res.status(404).json({message : "There is no tag with this id"});
+      return
+    }
+    res.status(200).json(tagData);
+  }catch (err) {
+    res.status(500).json(err);
+};
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
