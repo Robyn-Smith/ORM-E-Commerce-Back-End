1b728d69a6dc1013a252183244048cbaaa789767
Robyn Smith <robynsmith21@hotmail.com>
2023-08-05 23:24:38 +0100

    removed unneeded comment

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,7 +36,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new category --------------------change categoryData const name?
+  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
